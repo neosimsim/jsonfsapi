ebda6ebebd0f1d218cc82c08052d81c65e221c46
Alexander Ben Nasrallah <me@abn.sh>
2018-04-15 16:29:58 +0200

    Implement CreateElements

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"encoding/json"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -25,31 +25,75 @@ func main() {
 }
 
 func ServeElements(w http.ResponseWriter, req *http.Request) {
-	if req.Method == "GET" {
-		file := req.URL.Query().Get("uuid")
-		log.Print("Open ", file)
-		f, err := os.Open(file)
-		defer func() {
-			f.Sync()
-			f.Close()
-		}()
-		if err != nil {
-			log.Panic(err)
-		}
-		io.Copy(w, f)
-	} else if req.Method == "PUT" {
-		file := req.URL.Query().Get("uuid")
-		log.Print("Open ", file)
-		f, err := os.Create(file)
-		defer func() {
-			f.Sync()
-			f.Close()
-		}()
-		if err != nil {
-			log.Panic(err)
-		}
-		io.Copy(f, req.Body)
+	switch req.Method {
+	case "GET":
+		ReadElements(w, req)
+	case "POST":
+		CreateElements(w, req)
+	case "PUT":
+		UpdateElements(w, req)
+	case "DELETE":
+		DeleteElements(w, req)
 	}
+}
+
+func genUUID() string {
+	return "12123j1230123"
+}
+
+// Reads the JSON from the http.ResponseWriter deserialize to an interface{}
+// and sets the property UUID to a new UUID. A given UUID, if any, will we overwritten.
+// Encode the JSON to a file afterwards.
+func CreateElements(w http.ResponseWriter, req *http.Request) {
+	file := genUUID()
+	log.Print("Open ", file)
+	f, err := os.Create(file)
+	defer func() {
+		f.Sync()
+		f.Close()
+	}()
+	if err != nil {
+		log.Panic(err)
+	}
+	decoder := json.NewDecoder(req.Body)
+	var jsonObj interface{}
+	decoder.Decode(&jsonObj)
+	log.Print(jsonObj)
+	jsonObj.(map[string]interface{})["uuid"] = file
+	encoder := json.NewEncoder(f)
+	encoder.SetIndent("", "\t")
+	encoder.Encode(jsonObj)
+}
+
+func ReadElements(w http.ResponseWriter, req *http.Request) {
+	file := req.URL.Query().Get("uuid")
+	log.Print("Open ", file)
+	f, err := os.Open(file)
+	defer func() {
+		f.Sync()
+		f.Close()
+	}()
+	if err != nil {
+		log.Panic(err)
+	}
+	io.Copy(w, f)
+}
+
+func UpdateElements(w http.ResponseWriter, req *http.Request) {
+	file := req.URL.Query().Get("uuid")
+	log.Print("Open ", file)
+	f, err := os.Create(file)
+	defer func() {
+		f.Sync()
+		f.Close()
+	}()
+	if err != nil {
+		log.Panic(err)
+	}
+	io.Copy(f, req.Body)
+}
+
+func DeleteElements(w http.ResponseWriter, req *http.Request) {
 }
 
 func write() {
@@ -69,11 +113,11 @@ func write() {
 	}
 }
 
-func findByUUID(uuid string) (* os.File, error) {
+func findByUUID(uuid string) (*os.File, error) {
 	return findByProperty("uuid", fmt.Sprintf("^%s$", uuid))
 }
 
-func findByProperty(propertyPath, regexp string) (* os.File, error) {
+func findByProperty(propertyPath, regexp string) (*os.File, error) {
 	return nil, nil
 }
 
