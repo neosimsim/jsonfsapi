5aa94cb21119528b9aafe7e2d89f04f9664014b8
Alexander Ben Nasrallah <me@abn.sh>
2018-04-16 21:46:21 +0200

    Use cache repo in crud_test

--- crud_test.go
+++ crud_test.go
@@ -1,23 +1,50 @@
 package main
 
 import (
+	"net/http"
 	"net/http/httptest"
 	"strings"
 	"testing"
-	"log"
-	"os"
 )
 
 func TestCreate(t *testing.T) {
-	devNull, _ := os.Create(os.DevNull)
-	log.SetOutput(devNull)
-	req := httptest.NewRequest("POST", "http://localhost?uuid=deadbeef", strings.NewReader("{}"))
+	cache := Cache{}
+
+	req := httptest.NewRequest("POST", "http://localhost", strings.NewReader(`{ "title": "I'm new"}`))
 	w := httptest.NewRecorder()
-	ServeElementsFactory(NewFileRepo())(w, req)
+	ServeElementsFactory(&cache)(w, req)
 
 	resp := w.Result()
 
-	if resp.StatusCode != 201 {
-		t.Error("Expected status code ", 201, " got ", resp.StatusCode)
+	location := w.Header().Get("Location")
+	if location == "" {
+		t.Fatal("Location header is missing")
+	}
+
+	uuid := location
+	if _, exists := cache[uuid]; !exists {
+		t.Error("Element has not been stored to repo.")
+	}
+
+	if resp.StatusCode != http.StatusCreated {
+		t.Error("Expected status code ", http.StatusCreated, " got ", resp.StatusCode)
+	}
+}
+
+func TestDelete(t *testing.T) {
+	cache := Cache{"deadbeef": []byte("Hello World!")}
+
+	req := httptest.NewRequest("DELETE", "http://localhost?uuid=deadbeef", strings.NewReader("{}"))
+	w := httptest.NewRecorder()
+
+	ServeElementsFactory(&cache)(w, req)
+	resp := w.Result()
+
+	if _, exists := cache["deadbeef"]; exists {
+		t.Error("Element has not been deleted from repo.")
+	}
+
+	if resp.StatusCode != http.StatusNoContent {
+		t.Error("Expected status code ", http.StatusNoContent, " got ", resp.StatusCode)
 	}
 }
