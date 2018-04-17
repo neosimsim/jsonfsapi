a154563780a4d3cf3508b147837cfdb8d8879cb2
Alexander Ben Nasrallah <me@abn.sh>
2018-04-17 20:14:36 +0200

    Implement CRUD to match the test specs

--- crud.go
+++ crud.go
@@ -29,6 +29,7 @@ func CreateElements(repo Repo, w http.ResponseWriter, req *http.Request) {
 	encoder.SetIndent("", "\t")
 	encoder.Encode(jsonObj)
 	w.Header().Set("Location", file)
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
 }
 
@@ -53,20 +54,34 @@ func ReadElements(repo Repo, w http.ResponseWriter, req *http.Request) {
 	if err != nil {
 		log.Panic(err)
 	}
+	w.Header().Set("Content-Type", "application/json")
 	io.Copy(w, f)
 }
 
 func UpdateElements(repo Repo, w http.ResponseWriter, req *http.Request) {
-	file := req.URL.Query().Get("uuid")
-	log.Print("Open ", file)
-	f, err := repo.Writer(file)
+	uuid := req.URL.Query().Get("uuid")
+
+	log.Print("Open ", uuid)
+	f, err := repo.Writer(uuid)
 	defer func() {
 		f.Close()
 	}()
 	if err != nil {
 		log.Panic(err)
 	}
+
+	// make sure the UUID is not overwritten by the request
+	decoder := json.NewDecoder(req.Body)
+	var jsonObj map[string]interface{}
+	decoder.Decode(&jsonObj)
+	jsonObj["uuid"] = uuid
+
+	encoder := json.NewEncoder(f)
+	encoder.SetIndent("", "\t")
+	encoder.Encode(jsonObj)
 	io.Copy(f, req.Body)
+
+	w.WriteHeader(http.StatusNoContent)
 }
 
 func DeleteElements(repo Repo, w http.ResponseWriter, req *http.Request) {
